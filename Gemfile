88249482b7a18fc0c757e3fcdfad7b3c61fec5ae
Jason Wai <jason.a.wai@gmail.com>
2017-05-24 23:21:38 -0400

    first commit

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
 gem 'jquery-middleman'
 gem 'middleman-livereload'
 gem "middleman-blog", "~> 4.0"
+gem 'middleman-deploy', '~> 2.0.0.pre.alpha'
 
 gem 'redcarpet', '~> 3.3', '>= 3.3.3'
 
--- config.rb
+++ config.rb
@@ -69,11 +69,19 @@ page '/*.txt', layout: false
 #     'Helping'
 #   end
 # end
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
 
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
-
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+   activate :minify_css
+   activate :asset_hash
+   activate :minify_javascript
+   activate :css
+   activate :gzip
+end
